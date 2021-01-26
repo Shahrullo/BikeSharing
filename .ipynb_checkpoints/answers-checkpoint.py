955c7f8b31a45db85ce5eabfdacb1cc7d6487356
Shahrullo <shahrullo1995@mail.ru>
2021-01-26 09:07:48 +0900

    The training and testing have finished. The project showed good result

--- .ipynb_checkpoints/answers-checkpoint.py
+++ .ipynb_checkpoints/answers-checkpoint.py
@@ -140,7 +140,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 100
-learning_rate = 0.1
-hidden_nodes = 2
+iterations = 25000
+learning_rate = 0.25
+hidden_nodes = 25
 output_nodes = 1
